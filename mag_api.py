8bc07be6f706bd56464ad8a7e03ca1b589e1a17d
Jack Beasley <jack.beasley@gmail.com>
2018-11-05 17:07:52 -0800

    mag api file

---
+++ mag_api.py
@@ -0,0 +1,58 @@
+import requests
+import json
+import datetime
+import time
+
+
+def mag_evaluate(query, wait_time=10):
+    try:
+        response = requests.get(
+            url=
+            "http://api.labs.cognitive.microsoft.com/academic/v1.0/evaluate",
+            params={
+                "expr": query,
+                "model": "latest",
+                "count": "10",
+                "offset": "0",
+                #"orderby": "null",
+                "attributes": "Id,Ti,Y,CC,RId,D",
+            },
+            headers={
+                "Ocp-Apim-Subscription-Key":
+                "1c99573804ad445dac00b28fdbafbea3",
+                "subscription-key": "",
+            },
+        )
+
+        if response.status_code == 429:
+            # print("Rate limited: waiting {} seconds".format(wait_time))
+            time.sleep(wait_time)
+            return mag_evaluate(query, wait_time / 2)
+        wait_time = 1
+
+        # print('Response HTTP Status Code: {status_code}'.format(
+        #     status_code=response.status_code))
+
+        resp = json.loads(response.content)
+        if 'entities' in resp:
+            for obj in resp['entities']:
+                obj.pop('E', None)
+            return resp['entities']
+        else:
+            return resp
+    except requests.exceptions.RequestException:
+        print('HTTP Request failed')
+        return None
+
+
+def mag_get_title(mag_id):
+    obj = mag_evaluate("Id={}".format(mag_id))[0]
+    return str(obj['Ti']) + " (" + str(int(obj['Y'])) + ")"
+
+
+format_str = '%Y-%m-%d'  # The format
+
+
+def mag_get_date(mag_id):
+    obj = mag_evaluate("Id={}".format(mag_id))[0]
+    return datetime.datetime.strptime(obj['D'], format_str)
